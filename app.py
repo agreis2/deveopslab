ccd98f2ad6cd183433b019b0376146c9af0ecfe8
agreis2 <95056295+agreis2@users.noreply.github.com>
2022-02-19 18:36:24 -0600

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World- Ana Reis"
+    return "Hello World- Ana Reis test"
 
 if __name__ == '__main__':
     app.run()
